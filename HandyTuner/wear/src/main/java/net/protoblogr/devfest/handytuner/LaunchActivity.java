a7403204f94a574d261f46bab94f9c4c279e54fa
Jerry Jalava <jerry.jalava@iki.fi>
2014-10-05 13:59:58 +0300

    Initial commit

---
+++ HandyTuner/wear/src/main/java/net/protoblogr/devfest/handytuner/LaunchActivity.java
@@ -0,0 +1,24 @@
+package net.protoblogr.devfest.handytuner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class LaunchActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
